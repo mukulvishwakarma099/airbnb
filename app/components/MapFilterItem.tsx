77ffa654df82195343ad30c844430caf5fc6ffca
mukulvishwakarma099 <mukulvishwakarma099@gmail.com>
2024-03-18 13:35:37 +0530

    Setup prisma

--- app/components/MapFilterItem.tsx
+++ app/components/MapFilterItem.tsx
@@ -1,16 +1,38 @@
-import React from "react";
-import { categoryItems } from "../lib/categoryItems";
-import Link from "next/link";
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+import { categoryItems } from "../lib/categoryItems";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+import { cn } from "@/lib/utils";
 
 const MapFilterItem = () => {
+  const searchParams = useSearchParams();
+  const search = searchParams.get("filter");
+  const pathname = usePathname();
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
+
   return (
     <div className="flex gap-x-10 mt-5 w-full overflow-x-scroll no-scrollbar">
       {categoryItems.map((item) => (
         <Link
           key={item.id}
-          href=""
-          className="flex flex-col items-center gap-y-1"
+          href={pathname + "?" + createQueryString("filter", item.name)}
+          className={cn(
+            search === item.name
+              ? "border-b-2 border-black pb-2 flex-shrink-0"
+              : "opacity-70 flex-shrink-0",
+            "flex flex-col items-center gap-y-3"
+          )}
         >
           <div className="relative w-6 h-6">
             <Image
